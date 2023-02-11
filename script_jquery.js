488dc1ad39fce922ebd6a797fe90f87505db2858
Gracelene <43730881+Gracelene@users.noreply.github.com>
2023-02-10 21:22:18 -0300

    Add files via upload

---
+++ script_jquery.js
@@ -0,0 +1,37 @@
+//$(seletor).ação();
+
+//$(document).ready(function(){
+/*$(function(){
+	$('button').click(function(){
+		//$('h1').hide();
+		//$('h1').css("color", "red");
+		$('#unico').css("color", "red");
+
+		});
+
+});
+*/
+
+$(function(){
+	$('#azul').click(function(){
+		$('p').css("color", "blue");
+	});
+	$(function(){
+		$('#vermelho').click(function(){
+			$('p').css("color", "red");
+		});
+	});
+
+}); 
+
+/*$(function(){
+	$('#azul').click(function(){
+		$('p').css("background-color", "blue");
+	});
+	$(function(){
+		$('#vermelho').click(function(){
+			$('p').css("background-color", "red");
+		});
+	});
+
+}); */
